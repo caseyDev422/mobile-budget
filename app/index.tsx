a8e87df6e3565009900201699ac695924f071634
Sean Casey <seancasey076@gmail.com>
2026-02-09 18:37:49 -0600

    added in icons library, added buttons to login page

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,39 @@
 import { Text, View } from 'react-native';
 import { Container } from '@/components/Container';
+import { Button } from '@/components/Button';
+import EasyIcon from 'react-native-easy-icon';
 
 const Login = () => {
   return (
     <View className={styles.screen}>
       <Container className={styles.container}>
         <Text className={styles.signInText}>Sign in to your account to continue</Text>
+        <View className={styles.buttonContainer}>
+          <Button
+            title="Continue with Google"
+            iconSide='left'
+            icon={
+              <EasyIcon
+                type="antdesign"
+                name="google"
+                size={20}
+                color="#fff"
+              />
+            }
+          />
+          <Button
+            title="Continue with Microsoft"
+            icon={
+              <EasyIcon
+                type="font-awesome5"
+                name="microsoft"
+                size={20}
+                color="#fff"
+              />
+            }
+          />
+        </View>
+       
       </Container>
     </View>
   );
@@ -19,6 +47,9 @@ const styles = {
   screen: 'flex-1 p-12 justify-center',
   container: 'flex h-[75%] p-8',
   signInText: 'text-lg text-[#abb3ba]',
+  buttonContainer: 'mt-4',
+  button: 'p-2',
+  secondaryButton: 'mt-3',
   shadow: {
     shadowColor: '#000',
     shadowOffset: {
---
+++ react-native-vector-icons.d.ts
@@ -0,0 +1,8 @@
+declare module 'react-native-vector-icons/*' {
+  const IconComponent: any;
+  export default IconComponent;
+}
+
+declare module 'react-native-vector-icons/Icon' {
+  export class Icon {}
+}
